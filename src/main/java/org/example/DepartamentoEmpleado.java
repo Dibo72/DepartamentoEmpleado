dd8ef2fb92f2a7eb9524ced028290dcc74165c0b
Dibo72 <humanida133@gmail.com>
2026-03-24 10:56:29 +0100

    Ejercicio acabado

--- src/main/java/org/example/DepartamentoEmpleado.java
+++ src/main/java/org/example/DepartamentoEmpleado.java
@@ -1,17 +1,41 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class DepartamentoEmpleado {
+    public static void main(String[] args) {
+
+        try(Connection conn = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword());
+            Statement stmt = conn.createStatement()) {
+            String eliminarTablas = "DROP TABLE departamento CASCADE CONSTRAINTS";
+            String eliminar2 = "DROP TABLE empleado CASCADE CONSTRAINTS";
+            stmt.executeUpdate(eliminarTablas);
+            stmt.executeUpdate(eliminar2);
+
+            String sql = "CREATE TABLE departamento(" +
+                            "id VARCHAR(10) PRIMARY KEY, " +
+                            "nombre VARCHAR(25) NOT NULL)";
+            stmt.executeUpdate(sql);
+
+            System.out.println("Primera tabla insertada");
+
+            String empleado = "CREATE TABLE empleado(" +
+                                "id_Empleado VARCHAR(10) PRIMARY KEY," +
+                                " nombre VARCHAR(25) NOT NULL, " +
+                                "salario NUMBER(5) NOT NULL, " +
+                                "departamento_id VARCHAR(10), " +
+                                "FOREIGN KEY (DEPARTAMENTO_ID) REFERENCES departamento(id))";
+            stmt.executeUpdate(empleado);
+
+            System.out.println("Tablas creadas");
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
